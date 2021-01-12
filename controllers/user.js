9b4a7dccda7a84d0685bdd1d3cba1b32f42d2579
Gupy Wantoro <gupywant21@gmail.com>
2021-01-12 18:41:56 +0700

    creating user

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const validateToken = require('../Auth/auth.js').validateToken;
 let router = express.Router();
 
 router.get('/users',validateToken,userService.getAllUserList);
-router.post('/users/add',validateToken,userService.createUser);
+router.post('/users/add',userService.createUser);
 router.get('/user',validateToken,userService.getUserById);
 router.post('/user/update',validateToken,userService.updateUser);
 router.delete('/user/:id',validateToken,userService.deleteUser);
